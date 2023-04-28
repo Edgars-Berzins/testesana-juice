f85a0f5301b25d6b7debf7d0a634d94643e58408
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2023-04-28 13:14:06 +0300

    Changes to be committed: 	modified:   cypress/e2e/3-demoqa/elements.cy.js 	new file:   cypress/pageObjects/base.page.js 	new file:   cypress/pageObjects/checkBoxPage.page.js new file:   cypress/pageObjects/radioButtonPage.page.js

--- cypress/e2e/3-demoqa/elements.cy.js
+++ cypress/e2e/3-demoqa/elements.cy.js
@@ -1,10 +1,11 @@
 import TextBoxPage from "../../pageObjects/textBoxPage.page";
-
+import CheckboxPage from "../../pageObjects/checkBoxPage.page";
+import RadioButtonPage from "../../pageObjects/radioButtonPage.page";
 
 describe("Elements", () => {
   context("Text Box scenarios", () => {
     beforeEach(() => {
-        TextBoxPage.visit();
+      TextBoxPage.visit();
     });
 
     it("Text Box Positive", () => {
@@ -15,10 +16,10 @@ describe("Elements", () => {
       TextBoxPage.permanentAddressField.type("random Permanent Address");
       TextBoxPage.submitButton.click();
       //validation
-    TextBoxPage.nameSection.should("have.text", "Name:Donald Trump");
-    TextBoxPage.emailSection.should("have.text", "Email:random@random.lv");
-    TextBoxPage.currentSection.should("contain.text", "Current Address :random Current Address");
-    TextBoxPage.permanentSection.should(
+      TextBoxPage.nameSection.should("have.text", "Name:Donald Trump");
+      TextBoxPage.emailSection.should("have.text", "Email:random@random.lv");
+      TextBoxPage.currentSection.should("contain.text", "Current Address :random Current Address");
+      TextBoxPage.permanentSection.should(
         "contain.text",
         "Permananet Address :random Permanent Address"
       );
@@ -26,15 +27,78 @@ describe("Elements", () => {
 
 
 
-       
+
     });
-    
-    it ("Text Box Negative", () => {
-        TextBoxPage.emailField.should("not.have.class", "field-error");
-        TextBoxPage.emailField.type("random@email@asdsada.com");
-        TextBoxPage.submitButton.click();
-        TextBoxPage.emailField.should("have.class", "field-error");
+
+    it("Text Box Negative", () => {
+      TextBoxPage.emailField.should("not.have.class", "field-error");
+      TextBoxPage.emailField.type("random@email@asdsada.com");
+      TextBoxPage.submitButton.click();
+      TextBoxPage.emailField.should("have.class", "field-error");
+    });
+
+    // Checkbox scenarios
+    context("Check Box scenarios", () => {
+      beforeEach(() => {
+        CheckboxPage.visit();
       });
 
+      it('Checkbox select items', () => {
+        // 
+        CheckboxPage.expandAllButton.click();
+        CheckboxPage.checkboxTitles.contains("Notes").click();
+        CheckboxPage.checkboxTitles.contains("Angular").click();
+        CheckboxPage.checkboxTitles.contains("Private").click();
+        CheckboxPage.checkboxTitles.contains("Excel File.doc").click();
+        // Validate that selected items are registered
+        CheckboxPage.result.should(
+          "have.text",
+          "You have selected :notesangularprivateexcelFile"
+        );
+
+      })
+
+      it("Checkbox click Desktop", () => {
+        // Click Expand (+) icon/button
+        CheckboxPage.expandAllButton.click();
+        // Click Desktop
+        CheckboxPage.checkboxTitles.contains("Desktop").click();
+        // Validate success message - You have selected :desktopnotescommands
+        CheckboxPage.result.should(
+          "have.text",
+          "You have selected :desktopnotescommands"
+        );
+      });
+
+
+    });
   });
+  // Radio button
+  context("Radio Button scenarios", () => {
+    beforeEach(() => {
+      RadioButtonPage.visit();
+    });
+    it.only("Click buttons and validate", () => {
+      // Click Yes button
+      RadioButtonPage.selectButton.contains("Yes").click();
+      // Validate message - You have selected Yes
+      RadioButtonPage.result.should(
+        "have.text",
+        "Yes"
+      );
+      // Click Impressive button
+      RadioButtonPage.selectButton.contains("Impressive").click();
+      // Validate message - You have selected Impressive
+      RadioButtonPage.result.should(
+        "have.text",
+        "Impressive"
+      );
+      // Validate that no button is disabled
+      RadioButtonPage.noButton.should("have.class","disabled");
+
+
+    });
+  });
+
+
 });
